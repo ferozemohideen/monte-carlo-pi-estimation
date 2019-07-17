63a60479577074aec331e213dfeb398ef7fe2cfd
Feroze Mohideen <fmohidee@amazon.com>
2019-07-17 14:32:45 -0700

    added line chart below simulation window to track estimation of pi

--- monte-carlo.js
+++ monte-carlo.js
@@ -27,7 +27,13 @@ d3.select('.svg-container')
   .classed("button-container", true);
 
 var add_circle_button = d3.select(".button-container").append("button");
-add_circle_button.text("submit");
+add_circle_button.text("single step");
+
+var start_animation_button = d3.select(".button-container").append("button");
+start_animation_button.text("start simulation");
+
+var stop_animation_button = d3.select(".button-container").append("button");
+stop_animation_button.text("stop simulation");
 
 var pi_value = d3.select(".button-container").append("p");
 pi_value.text("None yet");
@@ -43,79 +49,128 @@ var estimates = [];
 var width = 400;
 var height = 400;
 
-var margin = { top: 20, right: 20, bottom: 20, left: 50 };
+var margin = { top: 50, right: 20, bottom: 50, left: 20 };
 
 var svgline = d3.select(".svg-container").append("svg")
   .attr("height", height).attr("width", width)
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.right + ")");
 
-var xScale = d3.scale.linear()
-  .range([0, width - margin.left - margin.right])
+var xScale = d3.scaleLinear()
+  .range([0, width - margin.left - margin.right]);
 
-var yScale = d3.scale.linear()
-  .range([height - margin.top - margin.bottom, 0])
+var yScale = d3.scaleLinear()
+  .range([height - margin.top - margin.bottom, 0]);
 
-var line = d3.svg.line().interpolate("monotone")
-  .x(function (d) { return xScale(d.x); })
-  .y(function (d) { return yScale(d.y); })
+var xAxisCall = d3.axisBottom();
+var yAxisCall = d3.axisLeft();
 
-function render() {
-  var data = estimates.map(function(item, index){ return {x: index, y: item};});
-  console.log(data);
-
-  var yMin = 0
-  var yMax = Math.max(estimates);
-
-  yScale.domain([yMin, yMax]);
-
-  var yAxis = d3.svg.axis().scale(yScale).orient("left");
-
-  svgline.selectAll(".y.axis").remove();
-
-  svgline.append("g").attr("class", "y axis").call(yAxis);
-
-  svgline.selectAll(".line").remove();
-
-  var lines = svgline.selectAll(".line").data(data).attr("class", "line");
-
-  lines.enter().append("path")
-    .attr("class", "line")
-    .attr("d", line).style("stroke", "blue");
-}
-
-(function () {
-  var svg = d3.select('svg');
-  function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-
-  function drawCircle(x, y, size) {
-    console.log('Drawing circle at', x, y, size);
-    x_adj = x - center[0];
-    y_adj = center[1] - y;
-    var distance = Math.sqrt(Math.pow(x_adj, 2) + Math.pow(y_adj, 2)) / center[0];
-    console.log('Distance = ', distance);
-    if (distance <= 1) {
-      var temp = svg.append("circle").attr('class', 'click-circle-blue').attr("cx", x).attr("cy", y);
-      incircle += 1;
-    }
-    else {
-      var temp = svg.append("circle").attr('class', 'click-circle-red').attr("cx", x).attr("cy", y);
-      outcircle += 1;
-    }
-    temp.transition().duration(500).attr("r", 10).transition(250).attr("r", 2);
-    var new_estimate = (incircle / (incircle + outcircle));
-    pi_value.text(new_estimate);
-    estimates.push(new_estimate);
-    render();
-  }
-
-  add_circle_button.on('click', function () {
-    var coords = [getRandom(0, 400), getRandom(0, 400)];
-    console.log(coords);
-    drawCircle(coords[0], coords[1], 2);
+var plotLine = d3.line()
+  .curve(d3.curveMonotoneX)
+  .x(function (d) {
+    return xScale(d.x);
+  })
+  .y(function (d) {
+    return yScale(d.y);
   });
 
+var line = svgline.append("g").append("path").attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+  .datum(estimates)
+  .attr("d", plotLine)
+  .style("fill", "none")
+  .style("stroke", "brown");
 
-})();
+initAxis();
+var max_estimate = -1;
+function initAxis() {
+  xScale.domain([0, 1]).range([0, width - (margin.top + margin.bottom)]);
+  yScale.domain([0, 1]).range([height - (margin.top + margin.bottom), 0]);
+  xAxisCall.scale(xScale);
+  yAxisCall.scale(yScale);
+
+  svgline.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(" + [margin.left, height - margin.top] + ")")
+    .call(xAxisCall);
+  svgline.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(" + [margin.left, margin.top] + ")")
+    .call(yAxisCall);
+}
+
+function updateAxis(count) {
+  xScale.domain([0, count]).range([0, width - (margin.top + margin.bottom)]);
+  xAxisCall.scale(xScale);
+  yScale.domain([0, max_estimate + 0.2]).range([height - (margin.top + margin.bottom), 0]);
+  yAxisCall.scale(yScale);
+
+
+  var t = d3.transition()
+    .duration(500);
+
+  line.datum(estimates.map(function (item, index) { return { x: index, y: item }; }))
+    .transition(t)
+    .attr("d", plotLine)
+    .style("fill", "none")
+    .style("stroke-width", "2px")
+    .style("stroke", "red");
+
+  svgline.select(".x")
+    .transition(t)
+    .call(xAxisCall);
+
+  svgline.select(".y")
+    .transition(t)
+    .call(yAxisCall)
+
+}
+
+
+var svg = d3.select('svg');
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function drawCircle(x, y, size) {
+  // console.log('Drawing circle at', x, y, size);
+  x_adj = x - center[0];
+  y_adj = center[1] - y;
+  var distance = Math.sqrt(Math.pow(x_adj, 2) + Math.pow(y_adj, 2)) / center[0];
+  // console.log('Distance = ', distance);
+  if (distance <= 1) {
+    var temp = svg.append("circle").attr('class', 'click-circle-blue').attr("cx", x).attr("cy", y);
+    incircle += 1;
+  }
+  else {
+    var temp = svg.append("circle").attr('class', 'click-circle-red').attr("cx", x).attr("cy", y);
+    outcircle += 1;
+  }
+  temp.transition().duration(500).attr("r", 10).transition(250).attr("r", 2);
+  var new_estimate = outcircle == 0 ? incircle : (incircle / outcircle);
+  pi_value.text("Current Estimate of pi: " + new_estimate.toFixed(2));
+  estimates.push(new_estimate);
+  //render();
+  if (new_estimate > max_estimate) {
+    max_estimate = new_estimate;
+  }
+  updateAxis(estimates.length);
+}
+
+function step() {
+  var coords = [getRandom(0, 400), getRandom(0, 400)];
+  // console.log(coords);
+  drawCircle(coords[0], coords[1], 2);
+}
+
+add_circle_button.on('click', function () {
+  step();
+});
+
+var interval;
+start_animation_button.on('click', function () {
+  interval = setInterval(step, 10);
+});
+
+stop_animation_button.on('click', function() {
+  clearInterval(interval);
+})
